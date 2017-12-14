78a00024d25d20c5390b7789533cd2f1c6bb58f2
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-12-14 10:52:00 +0100

    renamings

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nexta chart service' });
+  res.render('index', { title: 'Image-chart service' });
 });
 
 module.exports = router;
